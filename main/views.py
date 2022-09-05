151915294829110d878cca99ba8326d0177a5467
JangJiWook <swjw28@naver.com>
2022-09-05 20:45:44 +0900

    update models.py

--- main/views.py
+++ main/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
   return render(request, 'main/index.html')
 
 def blog(request):
-  return render(request, 'main/blog.html')
+  # 모든 Post를 가져와 postlist에 저장
+  postlist = Post.objects.all()
+  # blog.html 페이지를 열 때, 모든 Post인 postlist도 같이 가져옴
+  return render(request, 'main/blog.html', {'postlist':postlist})
